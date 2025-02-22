4d5aa6b26a5729140754b4cc82939a5cb6d64956
gkibria <gkibria121@gmail.com>
2025-02-22 19:56:55 +0600

    add text input

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,2 @@
+<input type={{$attributes['type']??"text"}} {{$attributes}} {{$attributes->class(['px-2 py-1 ring-1 rounded-md focus:ring-2 ring-slate-400 focus:ring-blue-400 outline-none'])}}   >
+ 
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,7 +2,76 @@
 
     <x-breadcrumbs :links="[ 'Jobs' => route('jobs.index')]"/>
 
+       
+     
+        
+
     <div class="max-w-[90vw]  pt-4">
+        <x-card class="mb-4 "> 
+            <form class="grid grid-cols-2 gap-2"> 
+            <x-form-group  >
+                <label for="search" class="font-bold">Search</label>
+                <x-text-input placeholder="Search for any text" name="search" />
+            </x-form-group>
+            <x-form-group>
+                <label for="salary" class="font-bold">Salary</label>
+                <div>
+                    <x-text-input placeholder="From"  type="number"   />
+                    <x-text-input placeholder="To" type="number" />
+                </div>
+                
+            </x-form-group>
+
+            <x-form-group >
+                <h1 class="font-bold" >Experience</h1>
+                <div>
+                    <input type="radio" name="experience" id="experience-1" value="" checked>
+                    <label for="experience-1">All</label>
+                </div>
+                <div>
+                    <input type="radio" name="experience" id="experience-2" value="">
+                    <label for="experience-2">Entry</label>
+                </div>
+                <div>
+                    <input type="radio" name="experience" id="experience-3" value="">
+                    <label for="experience-3">Intermediate</label>
+                </div>
+                <div>
+                    <input type="radio" name="experience" id="experience-4" value="">
+                    <label for="experience-4">Senior</label>
+                </div>
+                
+            </x-form-group>
+            <x-form-group >
+                <h1 class="font-bold" >Category</h1>
+                <div>
+                    <input type="radio" name="category" id="category-1" value="" checked>
+                    <label for="category-1">All</label>
+                </div>
+                <div>
+                    <input type="radio" name="category" id="category-2" value="">
+                    <label for="category-2">IT</label>
+                </div>
+                <div>
+                    <input type="radio" name="category" id="category-3" value="">
+                    <label for="category-3">Finance</label>
+                </div>
+                <div>
+                    <input type="radio" name="category" id="category-4" value="">
+                    <label for="category-4">Sales</label>
+                </div>
+                <div>
+                    <input type="radio" name="category" id="category-4" value="">
+                    <label for="category-4">Marketing</label>
+                </div>
+                
+            </x-form-group>
+
+            <button class="w-full col-span-2 mt-3" type="submit" >Filter</button>
+        </form> 
+       
+            
+        </x-card>
         @forelse ($jobs as $job)
             <x-job-card class="mb-4" :$job> 
                 <x-link href="{{ route('jobs.show', ['job' => $job]) }}">Show</x-link> 
