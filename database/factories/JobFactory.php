cc3cc56032842e41688f574fd46ff48b07f9fd69
gkibria <gkibria121@gmail.com>
2025-02-22 08:12:56 +0600

    add jobs table

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'  => fake()->jobTitle(),
+            'salary' => random_int(10000, 100000),
+            'location'  => fake()->city(),
+            'category' => fake()->randomElement(Job::$categories),
+            'experience' => fake()->randomElement(Job::$experience),
+        ];
+    }
+}
